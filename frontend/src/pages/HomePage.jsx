ce72adbb7d86b13650b19a0788cf4c453b4845e3
MartinKamburov <martin.kamburov@yahoo.ca>
2025-06-20 17:25:27 -0400

    Fixed @component import error

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 
-import Navbar from '../components/Navbar'
-import SearchBar from '../components/SearchBar'
-import Footer from '../components/Footer'
-import FAQ from '../components/FAQ';
+import Navbar from '@components/Navbar'
+import SearchBar from '@components/SearchBar'
+import Footer from '@components/Footer'
+import FAQ from '@components/FAQ';
 
 
 export default function HomePage() {
   const [handleSearch, setHandleSearch] = useState('');
+
+
 
   return (
     <>
